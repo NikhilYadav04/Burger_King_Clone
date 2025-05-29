a2a92b223e499027ee81ea3a4f125430ac425ee7
NikhilYadav04 <byadav1723@gmail.com>
2025-05-30 00:19:46 +0530

    initialzed core folder

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color backGround = Color(0xFFF8EBDB);
+  static const Color brownColor = Color(0xFFF8EBDB);
+  static const Color switchColor = Color(0xFFEE7700);
+  static const Color lineColor = Color(0xFF723A1C);
+  static const Color cardColor = Color(0xFFFFFCF7);
+  static const Color backGroundA = Color(0xFFF3DFC6);
+  static const Color lightGrayColor = Color(0xFFF7F6F2);
+  static const Color lightGreenColor = Color(0xFF72BA00);
+}
--- lib/models/order/model_order.dart
+++ lib/models/order/model_order.dart
@@ -1,20 +1,17 @@
 class OrderModel {
   final String Name;
   final String Desc;
-   double Price;
-   double PriceRep;
-   int index = 1;
+  double Price;
+  double PriceRep;
+  int index = 1;
   final String Mealtype;
-  
-  
 
-  OrderModel({
-    required this.Name,
-    required this.Desc,
-    required this.Price,
-    required this.PriceRep,
-    required this.Mealtype
-  });
+  OrderModel(
+      {required this.Name,
+      required this.Desc,
+      required this.Price,
+      required this.PriceRep,
+      required this.Mealtype});
 
   @override
   String toString() {
